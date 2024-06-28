fab330a08383bc2c4fe3bfd61f3d01c36dbfb782
Raymond Htet <htetthurazaw@gmail.com>
2024-06-29 00:43:20 +0800

    added access key, secret key and region

--- src/main/java/com/quemistry/question_ms/config/DynamoDBConfig.java
+++ src/main/java/com/quemistry/question_ms/config/DynamoDBConfig.java
@@ -1,32 +1,46 @@
 package com.quemistry.question_ms.config;
 
-import com.amazonaws.ClientConfigurationFactory;
-import com.amazonaws.Protocol;
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableDynamoDBRepositories(basePackages = "com.quemistry.question_ms.repository")
 public class DynamoDBConfig {
+
+    private final String amazonAWSAccessKey;
+
+    private final String amazonAWSSecretKey;
+
+    private final String amazonAWSRegion;
+
+    public DynamoDBConfig(
+            @Value("${amazon.aws.accesskey}") String amazonAWSAccessKey,
+            @Value("${amazon.aws.secretkey}") String amazonAWSSecretKey,
+            @Value("${amazon.aws.region}") String amazonAWSRegion) {
+        this.amazonAWSAccessKey = amazonAWSAccessKey;
+        this.amazonAWSSecretKey = amazonAWSSecretKey;
+        this.amazonAWSRegion = amazonAWSRegion;
+    }
+
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
-
         return AmazonDynamoDBClientBuilder
-                .standard()
-                .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
-                .withRegion("ap-southeast-1")
+                .standard().
+                withCredentials(new AWSStaticCredentialsProvider(amazonAWSCredentials()))
+                .withRegion(amazonAWSRegion)
                 .build();
     }
 
-//    @Bean
-//    public AmazonDynamoDB amazonDynamoDB() {
-//        return AmazonDynamoDBClientBuilder.standard()
-//                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "ap-southeast-1"))
-//                .build();
-//    }
+    @Bean
+    public AWSCredentials amazonAWSCredentials() {
+        return new BasicAWSCredentials(
+                amazonAWSAccessKey, amazonAWSSecretKey);
+    }
 }
--- src/test/java/com/quemistry/question_ms/ApplicationTests.java
+++ src/test/java/com/quemistry/question_ms/ApplicationTests.java
@@ -2,8 +2,15 @@ package com.quemistry.question_ms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"amazon.aws.accesskey=testValue",
+		"amazon.aws.secretkey=testValue",
+		"amazon.aws.region=testValue"
+})
 class ApplicationTests {
 
 	@Test
