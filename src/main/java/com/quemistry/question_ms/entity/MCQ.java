854b140cf3d70c89512d51765386b414f2560e6c
ernestjx <73307945+ernestjx@users.noreply.github.com>
2024-07-14 21:53:52 +0800

    updated id generation strategy

--- src/main/java/com/quemistry/question_ms/entity/MCQ.java
+++ src/main/java/com/quemistry/question_ms/entity/MCQ.java
@@ -22,7 +22,7 @@ import java.util.List;
 public class MCQ {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
 
--- src/main/java/com/quemistry/question_ms/entity/Skill.java
+++ src/main/java/com/quemistry/question_ms/entity/Skill.java
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 public class Skill {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
