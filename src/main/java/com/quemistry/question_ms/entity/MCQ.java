5f40636bbd450a197de8a9a0733c970c1a6b4a07
Wang Shiji <scglwsj@163.com>
2024-07-27 00:12:58 +0800

    add the schema to the table

--- src/main/java/com/quemistry/question_ms/entity/MCQ.java
+++ src/main/java/com/quemistry/question_ms/entity/MCQ.java
@@ -2,14 +2,7 @@ package com.quemistry.question_ms.entity;
 
 import com.quemistry.question_ms.model.QuestionOption;
 import com.quemistry.question_ms.util.OptionConverter;
-import jakarta.persistence.Convert;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,6 +11,7 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
+@Table(name = "mcq", schema = "qms_question")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -38,6 +32,7 @@ public class MCQ {
     @ManyToMany
     @JoinTable(
             name = "mcq_topics",
+            schema = "qms_question",
             joinColumns = @JoinColumn(name = "mcq_id"),
             inverseJoinColumns = @JoinColumn(name = "topic_id")
     )
@@ -46,6 +41,7 @@ public class MCQ {
     @ManyToMany
     @JoinTable(
             name = "mcq_skills", // Ensure this is the correct join table name
+            schema = "qms_question",
             joinColumns = @JoinColumn(name = "mcq_id"),
             inverseJoinColumns = @JoinColumn(name = "skill_id")
     )
