db13366345f4ea92e9057942e381ee80a77073f3
Wang Shiji <scglwsj@163.com>
2024-07-29 01:57:07 +0800

    add schema

--- src/main/java/com/quemistry/question_ms/entity/Skill.java
+++ src/main/java/com/quemistry/question_ms/entity/Skill.java
@@ -1,11 +1,6 @@
 package com.quemistry.question_ms.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "skill", schema = "qms_question")
 public class Skill {
 
     @Id
