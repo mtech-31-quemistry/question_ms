86bcec19aedc9eefdb5deea7881dc3fb740eea00
ernestjx <73307945+ernestjx@users.noreply.github.com>
2024-08-10 10:02:17 +0800

    added PATCH to allowed methods

--- src/main/java/com/quemistry/question_ms/config/CorsConfig.java
+++ src/main/java/com/quemistry/question_ms/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "OPTIONS")
+                .allowedMethods("GET", "POST", "OPTIONS","PATCH")
                 .allowCredentials(true)// You can set this to true if you need credentials (cookies, etc.) to be included in the request.
                 .maxAge(3600);// Cache preflight request for 1 hour
     }
